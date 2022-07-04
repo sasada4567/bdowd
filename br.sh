a0e9c396d742e02ceff77cae88edeaf6aacb16cc
sasada4567 <53123563+sasada4567@users.noreply.github.com>
2022-07-04 12:01:27 +0300

    Add files via upload

---
+++ br.sh
@@ -0,0 +1,6 @@
+npm i request
+npm i puppeteer-extra-plugin-stealth
+npm i puppeteer-extra
+npm i user-agents
+npm i puppeteer-extra-plugin-recaptcha &&
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt update; sudo apt upgrade && sudo apt-get install ./google-chrome-stable_current_amd64.deb
